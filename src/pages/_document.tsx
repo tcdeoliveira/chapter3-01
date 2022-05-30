32e17675485040d059d3bff63ed7ee6be3c25c97
Tiago Oliveira <tiagooliveira@takethewind.com>
2022-05-30 21:58:40 +0100

    final

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render(): JSX.Element {
+  render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
@@ -14,6 +16,11 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=spacetraveling2"
+          />
         </body>
       </Html>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+/* eslint-disable react/no-danger */
 import { GetStaticProps } from 'next';
-import { RichText } from 'prismic-dom';
+import Head from 'next/head';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import Link from 'next/link';
-
-import { FiUser, FiCalendar } from 'react-icons/fi';
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -16,136 +22,190 @@ interface Post {
     title: string;
     subtitle: string;
     author: string;
+    readTime: number;
+    content: {
+      heading: string;
+      body: {
+        text: string;
+      }[];
+    }[];
   };
 }
+
 interface PostPagination {
   next_page: string;
   results: Post[];
 }
 
-interface IPrismicResponse {
-  page: number;
-  results_per_page: number;
-  results_size: number;
-  total_results_size: number;
-  total_pages: number;
-  next_page: string | null;
-  prev_page: string | null;
-  results: [];
-}
-
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-export default function Home({ postsPagination }: HomeProps): JSX.Element {
-  const [posts, setPosts] = useState([...postsPagination.results]);
-  const [nextPostsPage, setNextPostsPage] = useState(postsPagination.next_page);
-  const [next, setNext] = useState(postsPagination.next_page);
-  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+export default function Home({
+  postsPagination,
+  preview,
+}: HomeProps): ReactElement {
+  function getReadTime(item: Post): number {
+    const totalWords = item.data.content.reduce((total, contentItem) => {
+      total += contentItem.heading?.split(' ').length;
 
-  async function onClickNextPosts(): Promise<void> {
-    if (nextPage === null) {
+      const words = contentItem.body.map(i => i.text.split(' ').length);
+      words.map(word => (total += word));
+      return total;
+    }, 0);
+    return Math.ceil(totalWords / 200);
+  }
+
+  const formattedPost = postsPagination.results.map(post => {
+    const readTime = getReadTime(post);
+
+    return {
+      ...post,
+      data: {
+        ...post.data,
+        readTime,
+      },
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
       return;
     }
-    const response = await fetch(nextPostsPage);
-    const responseData = await response.json();
-    const nextPosts = responseData.results.map((post): Post => {
+
+    const postsResults = await fetch(`${nextPage}`).then(response =>
+      response.json()
+    );
+    setNextPage(postsResults.next_page);
+    setCurrentPage(postsResults.page);
+
+    const newPosts = postsResults.results.map((post: Post) => {
+      const readTime = getReadTime(post);
+
       return {
         uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
         data: {
           title: post.data.title,
           subtitle: post.data.subtitle,
           author: post.data.author,
+          readTime,
         },
-        first_publication_date: new Date(
-          post.first_publication_date
-        ).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
       };
     });
-    setPosts([...posts, ...nextPosts]);
-    setNextPage(responseData.next_page);
+
+    setPosts([...posts, ...newPosts]);
   }
 
   return (
     <>
-      <header className={`${commonStyles.container} ${styles.header}`}>
-        <div className={commonStyles.container__content}>
-          <img src="/images/Logo.svg" alt="Logo" />
-        </div>
-      </header>
-      <section className={`${commonStyles.container} ${styles.section}`}>
-        {posts.map((post: Post) => (
-          <article key={post.uid} className={commonStyles.container__content}>
-            <Link href={post.uid}>
-              <a>
-                <h3> {post.data.title} </h3>
-              </a>
-            </Link>
-            <Link href={post.uid}>
-              <a>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
                 <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                  <li>
+                    <FiClock />
+                    {`${post.data.readTime} min`}
+                  </li>
+                </ul>
               </a>
             </Link>
-            <div>
-              <time>
-                <FiCalendar /> {post.first_publication_date}
-              </time>
-              <span>
-                <FiUser /> {post.data.author}
-              </span>
-            </div>
-          </article>
-        ))}
-        <div className={commonStyles.container__content}>
+          ))}
+
           {nextPage && (
-            <button
-              type="button"
-              onClick={onClickNextPosts}
-              className={styles.load_more_posts_button}
-            >
+            <button type="button" onClick={handleNextPage}>
               Carregar mais posts
             </button>
           )}
         </div>
-      </section>
+
+        {preview && (
+          <aside>
+            <Link href="/api/exit-preview">
+              <a className={commonStyles.preview}>Sair do modo Preview</a>
+            </Link>
+          </aside>
+        )}
+      </main>
     </>
   );
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const getStaticProps = async () => {
-  const prismic = getPrismicClient({});
-  const response = await prismic.getByType('posts', { pageSize: 2 });
-  const posts = response.results.map((post): Post => {
+export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 3,
+      orderings: '[document.last_publication_date desc]',
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
     return {
       uid: post.uid,
+      first_publication_date: post.first_publication_date,
       data: {
         title: post.data.title,
         subtitle: post.data.subtitle,
         author: post.data.author,
+        content: post.data.content.map(content => {
+          return {
+            heading: content.heading,
+            body: [...content.body],
+          };
+        }),
       },
-      first_publication_date: new Date(
-        post.first_publication_date
-      ).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      }),
     };
   });
+
   const postsPagination = {
-    next_page: response.next_page,
+    next_page: postsResponse.next_page,
     results: posts,
   };
+
   return {
     props: {
       postsPagination,
+      preview,
     },
+    revalidate: 1800,
   };
 };
