eaffe925460456d0cdffe7a826ffa1e3b3dc6d39
Tiago Oliveira <tiagoclaudio@outlook.com>
2022-06-11 18:08:14 +0100

    [Change]Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import commonStyles from '../../styles/common.module.scss';
 import styles from './header.module.scss';
+import Link from 'next/link';
 
 export default function Header() {
   return(
     <header className={`${commonStyles.container} ${styles.header}`}>
-      <div className={commonStyles.container__content}>
-        <img src="/images/Logo.svg" alt="Logo" />
-      </div>
+        <div className={commonStyles.container__content}>
+          <Link href='/'>
+            <a className={styles.header_logo}>
+              <img src="/images/Logo.svg" alt="Logo" />
+            </a>
+          </Link>
+        </div>
     </header>
   );
 }
